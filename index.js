cbd3c484a4c085437e670c60bcfa78838677c51d
Eric Jaucian <eric.jaucian@gmail.com>
2024-05-04 09:36:17 +0800

    api route updated

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/addPhLvl',async(req,res)=>{
  
 })
 
-app.post('/ ',async(req,res)=>{
+app.post('/waterLevels',async(req,res)=>{
   console.log(req.body.data)
   const { error } = await supabase
   .from('waterLevels')
